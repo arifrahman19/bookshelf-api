8fe2803f8637a3f7aa511599ccf040f82e3d9668
arif rahman <arifrahman1997.aa@gmail.com>
2021-05-01 14:37:46 +0700

    first init

---
+++ src/handler.js
@@ -0,0 +1,61 @@
+const { nanoid } = require('nanoid')
+const books = require('./books')
+
+const addBooksHandler = (request, h) => {
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
+
+  const id = nanoid(16)
+  const insertedAt = new Date().toISOString()
+  const updatedAt = insertedAt
+  let finished
+  if (pageCount === readPage) {
+    finished = true
+  } else {
+    finished = false
+  }
+
+  const newBooks = {
+    id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt
+  }
+
+  if (newBooks.name === '' || newBooks.name === null) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku'
+    })
+    response.code(400)
+    return response
+  } else if (newBooks.readPage > newBooks.pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+    })
+    response.code(400)
+    return response
+  }
+
+  books.push(newBooks)
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id
+      }
+    })
+    response.code(201)
+    return response
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan'
+  })
+  response.code(500)
+  return response
+}
+
+module.exports = addBooksHandler
